80559bdd9d1183499ee555cae106a744423933a6
Matheus Keidy <matheuskeidygomes@gmail.com>
2022-07-19 09:08:05 -0300

    att

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -5,16 +5,11 @@ import { UserContext } from '../../context/UserProvider';
 export default function Profile(props) {
 
     const context = useContext(UserContext);
-    const [user, setUser] = useState([]);
-
-    useEffect(() => {
-        setUser(context.user);
-    }, [context.user]);
 
     return <>
 
     <div className="profile-content">
-        <ReadMe user={user} />
+        <ReadMe user={context.user} />
     </div>
     
     </>
